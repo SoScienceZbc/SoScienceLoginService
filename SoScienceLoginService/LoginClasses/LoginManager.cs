0e21c3993ad48ef3f2f75622f73544266330fa7e
Morten Vestergaard <mort286f@zbc.dk>
2022-06-01 08:37:36 +0200

    DirectoryEntry try

--- SoScienceLoginService/LoginClasses/LoginManager.cs
+++ SoScienceLoginService/LoginClasses/LoginManager.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.DirectoryServices;
 
 namespace SoScienceLoginService.LoginClasses
 {
@@ -21,34 +22,51 @@ namespace SoScienceLoginService.LoginClasses
             LoginRepley loginRepley = new LoginRepley();
             //LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("dc01.efif.dk", 389);
             Console.WriteLine("Before Ldap");
-            LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("10.255.1.1", 389);
+            //LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("10.255.1.1", 636);
             Console.WriteLine("After Ldap Identifier");
+           
             username = username.Split('@')[0];
             Console.WriteLine("Before Ldap Connection");
-            LdapConnection connection = new LdapConnection(identifier)
-            {
-                Credential = new NetworkCredential(username+"@zbc.dk", password)
-            };
-            Console.WriteLine("After Ldap Connection");
+            //LdapConnection connection = new LdapConnection(identifier)
+            //{
+            //    Credential = new NetworkCredential(username + "@zbc.dk", password)
+            //};
+
+            Console.WriteLine("just before DirectoryEntry connection");
+            DirectoryEntry connection = new DirectoryEntry("LDAP://10.255.1.1:389", username, password);
             try
             {
-                connection.Bind();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{username} have been login...");
-                Console.ForegroundColor = ConsoleColor.White;
-                loginRepley.LoginSucsefull = true;
+                var tmp = connection.NativeObject;
+                var temp = connection.Guid;
+                Console.WriteLine("conn works");
             }
-            catch
+            catch (Exception ex)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{username} cant login...");
-                Console.ForegroundColor = ConsoleColor.White;
-                loginRepley.LoginSucsefull = false;
+                Console.WriteLine("exception: " + ex.Message);
+                Console.WriteLine("conn fails");
+                throw;
             }
-            finally
-            {
-                connection.Dispose();
-            }
+            
+            //Console.WriteLine("After Ldap Connection");
+            //try
+            //{
+            //    //connection.Bind();
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine($"{username} have been login...");
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    loginRepley.LoginSucsefull = true;
+            //}
+            //catch
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine($"{username} cant login...");
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    loginRepley.LoginSucsefull = false;
+            //}
+            //finally
+            //{
+            //    connection.Dispose();
+            //}
 
 
 
