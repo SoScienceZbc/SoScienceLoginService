ea720c21d5135e119dbce9e94ff3ec700c19d507
Morten Vestergaard <mort286f@zbc.dk>
2022-06-21 07:26:46 +0200

    temporary login

--- SoScienceLoginService/LoginClasses/LoginManager.cs
+++ SoScienceLoginService/LoginClasses/LoginManager.cs
@@ -12,6 +12,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.DirectoryServices;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace SoScienceLoginService.LoginClasses
 {
@@ -21,19 +22,24 @@ namespace SoScienceLoginService.LoginClasses
         {
             Console.WriteLine(username, password);
             LoginRepley loginRepley = new LoginRepley();
-            //LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("dc01.efif.dk", 389);
-            Console.WriteLine("Before Ldap");
-            var cert = new X509Certificate("/home/soscience/Desktop/Services/LoginService/LDAPS_Certs/EFIF_Root_CA.crl");
-            LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("dc01.efif.dk", 636);
-            Console.WriteLine("After Ldap Identifier");
-           
-            username = username.Split('@')[0];
-            NetworkCredential credential = new NetworkCredential(username + "@zbc.dk", password);
-            Console.WriteLine("Before Ldap Connection");
-            LdapConnection connection = new LdapConnection(identifier, credential, AuthType.External);
-            connection.SessionOptions.SecureSocketLayer = true;
-            connection.ClientCertificates.Add(cert);
-            connection.SessionOptions.ProtocolVersion = 3;
+            ////LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("dc01.efif.dk", 389);
+            //Console.WriteLine("Before Ldap");
+            ////string[] pw = File.ReadAllLines("/home/soscience/Desktop/Services/PassPhrase.txt");
+            ////string pwToCheck = string.Concat(pw);
+            ////var cer = new X509Certificate("/home/soscience/Desktop/Services/soscience.dk.pfx", pwToCheck);
+            //X509Certificate cert = X509Certificate.CreateFromCertFile("/home/soscience/Desktop/Services/LoginService/LDAPS_Certs/EFIF - Root CA.crl");
+            //LdapDirectoryIdentifier identifier = new LdapDirectoryIdentifier("10.255.1.1", 636);
+            //Console.WriteLine("After Ldap Identifier");
+            //X509Certificate2 x509 = new X509Certificate2();
+            //byte[] rawData = ReadFile("/hom/soscience/Desktop/Services/LoginService/LDAPS_Certs/EFIF - Issuing CA+.crl");
+            //x509.Import(rawData);
+            //username = username.Split('@')[0];
+            //NetworkCredential credential = new NetworkCredential(username + "@zbc.dk", password);
+            //Console.WriteLine("Before Ldap Connection");
+            //LdapConnection connection = new LdapConnection(identifier, credential, AuthType.External);
+            //connection.SessionOptions.SecureSocketLayer = true;
+            //connection.ClientCertificates.Add(x509);
+            ////connection.SessionOptions.ProtocolVersion = 3;
 
 
 
@@ -55,26 +61,26 @@ namespace SoScienceLoginService.LoginClasses
             //    throw;
             //}
 
-            Console.WriteLine("After Ldap Connection");
-            try
-            {
-                connection.Bind();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{username} have been login...");
-                Console.ForegroundColor = ConsoleColor.White;
-                loginRepley.LoginSucsefull = true;
-            }
-            catch
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{username} cant login...");
-                Console.ForegroundColor = ConsoleColor.White;
-                loginRepley.LoginSucsefull = false;
-            }
-            finally
-            {
-                connection.Dispose();
-            }
+            //Console.WriteLine("After Ldap Connection");
+            //try
+            //{
+            //    connection.Bind();
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine($"{username} have been login...");
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    loginRepley.LoginSucsefull = true;
+            //}
+            //catch
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine($"{username} cant login...");
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    loginRepley.LoginSucsefull = false;
+            //}
+            //finally
+            //{
+            //    connection.Dispose();
+            //}
 
 
 
@@ -90,6 +96,15 @@ namespace SoScienceLoginService.LoginClasses
 
 
             return loginRepley;
+        }
+        internal static byte[] ReadFile(string fileName)
+        {
+            FileStream f = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            int size = (int)f.Length;
+            byte[] data = new byte[size];
+            size = f.Read(data, 0, size);
+            f.Close();
+            return data;
         }
     }
 }
